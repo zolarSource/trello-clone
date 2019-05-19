ca72d88aab3b6ee04172ef3955f02c983e1cb47c
unknown <lololo@example.com>
2019-05-19 21:53:38 +0300

    initial commit

---
+++ client/build/precache-manifest.72a87a05cb21bf8dc410726789968144.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "989f125988f91feb4511",
+    "url": "/static/js/main.989f1259.chunk.js"
+  },
+  {
+    "revision": "4bac82f087a33923d9c6",
+    "url": "/static/js/1.4bac82f0.chunk.js"
+  },
+  {
+    "revision": "989f125988f91feb4511",
+    "url": "/static/css/main.2bef3b2e.chunk.css"
+  },
+  {
+    "revision": "f2ee0868fe8dc51784b9041a47729d1b",
+    "url": "/index.html"
+  }
+];
